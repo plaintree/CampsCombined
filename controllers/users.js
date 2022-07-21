89db17abf827d9b30e0e0b9986e84801385ba223
Ryan Lui <ryanluiii@USERs-MacBook-Air.local>
2022-07-21 19:06:44 +0100

    rename yelp camp to campscombined

--- controllers/users.js
+++ controllers/users.js
@@ -5,51 +5,51 @@ const User = require("../models/user");
 
 //Register user
 module.exports.regUser = async (req, res, next) => {
-	try {
-		res.render("users/register");
-	} catch (err) {
-		next(err);
-	}
+  try {
+    res.render("users/register");
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports.regUserPost = async (req, res, next) => {
-	try {
-		const { email, username, password } = req.body;
-		const user = new User({ email, username });
-		const regUser = await User.register(user, password);
-		//Passport login syntax
-		req.login(user, function (err) {
-			if (err) {
-				return next(err);
-			}
-			req.flash("success", "Welcome to Yelp Camp");
-			res.redirect("/campgrounds");
-		});
-	} catch (error) {
-		req.flash("error", error.message);
-		res.redirect("/register");
-	}
+  try {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const regUser = await User.register(user, password);
+    //Passport login syntax
+    req.login(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "Welcome to CampsCombined");
+      res.redirect("/campgrounds");
+    });
+  } catch (error) {
+    req.flash("error", error.message);
+    res.redirect("/register");
+  }
 };
 
 //Login User
 module.exports.loginUser = async (req, res, next) => {
-	try {
-		res.render("users/login");
-	} catch (err) {
-		next(err);
-	}
+  try {
+    res.render("users/login");
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports.loginUserPost = (req, res) => {
-	const redirectURL = req.session.returnTo || "/campgrounds";
-	req.flash("success", "Welcome to Yelp Camp");
-	delete req.session.returnTo;
-	res.redirect(redirectURL);
+  const redirectURL = req.session.returnTo || "/campgrounds";
+  req.flash("success", "Welcome to CampsCombined");
+  delete req.session.returnTo;
+  res.redirect(redirectURL);
 };
 
 //Logout User
 module.exports.logoutUser = (req, res) => {
-	req.logout();
-	req.flash("success", "Successfully logged out.");
-	res.redirect("/campgrounds");
+  req.logout();
+  req.flash("success", "Successfully logged out.");
+  res.redirect("/campgrounds");
 };
